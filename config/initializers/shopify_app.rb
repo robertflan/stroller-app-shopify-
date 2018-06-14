5c24feecde4e7a2625ae5e76fe67b31ec00d7d87
@smart-web-rock <faonestar514@gmail.com>
2018-06-14 14:52:52 -0700

    update key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "d7a3394c20dc84d0993e0b84e8d127b2"
-  config.secret = "d7a3394c20dc84d0993e0b84e8d127b2"
+  config.secret = "d98b292d1bc97a2ca1a11e02db000620"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
