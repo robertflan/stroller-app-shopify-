89332f7af2a4b3dbc7d0629ab861b0fe16097b93
@smart-web-rock <faonestar514@gmail.com>
2018-06-15 05:04:19 -0700

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 30 })
+    @products = ShopifyAPI::Product.where(:product_type =>'custom_build')
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
