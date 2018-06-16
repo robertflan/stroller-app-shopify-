a5c1933b8e93d45972d3cf4f3d7f9a0f6fe0ec3d
@smart-web-rock <faonestar514@gmail.com>
2018-06-15 20:44:05 -0700

    change scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "d7a3394c20dc84d0993e0b84e8d127b2"
   config.secret = "d98b292d1bc97a2ca1a11e02db000620"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, write_products"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
